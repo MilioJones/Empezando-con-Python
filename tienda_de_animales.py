616f571189ce7166201b20b03b4167ebcc8f25e6
MilioJones <emilioruba@gmail.com>
2025-09-02 19:40:43 +0200

    Add files via upload

--- tienda_de_animales.py
+++ tienda_de_animales.py
@@ -5,40 +5,64 @@ print("************************************")
 num_perros = 10
 num_gatos = 8
 num_pajaros = 25
+nombre = ""
+apellido = ""
+respuesta = 1
+perros_adoptados = 0
+gatos_adoptados = 0
+pajaros_adoptados = 0
 
 animales_totales = num_perros + num_gatos + num_pajaros
 
-print("Ingresa tu nombre:")
-nombre = input()
-print("Ingresa tu apellido:")
-apellido = input()
+def menu():
+    print(" ")
+    print("1. Ver animales disponibles")
+    print("2. Adoptar un animal")
+    print("3. Salir")
+    opcion1=int(input("Opción: "))
+    return (opcion1)
+
+def ver_animales():
+    print("Actualmente contamos con:")
+    print(" ")
+    print(f"{num_perros} perros, {num_gatos} gatos y {num_pajaros} pájaros")
+    print(f"En total tenemos {animales_totales} animales disponibles para la adopción")
+
+def adopcion():
+    print("¿Qué tipo de animal deseas adoptar?")
+    print("1. Perro")
+    print("2. Gato")
+    print("3. Pájaro")
+    opcion2 = int(input("Opción: "))
+    return (opcion2)
+
+while nombre == "":
+    print("Ingresa tu nombre:")
+    nombre = input()
+while apellido == "":
+    print("Ingresa tu apellido:")
+    apellido = input()
 
 #concatenacion
 nombre_completo = nombre + " " + apellido
 
+print(" ")
 print(f"Gracias por visitarnos {nombre_completo}")
 
-print("Selecciona la opción que deseas:")
-print("1. Ver animales disponibles")
-print("2. Adoptar un animal")
-print("3. Salir")
-respuesta = int(input())
 
 while respuesta == 1 or respuesta == 2:
+    respuesta=menu()
+    
     if respuesta == 1:
-        print("Actualmente contamos con:")
-        print(f"{num_perros} perros, {num_gatos} gatos y {num_pajaros} pájaros")
-        print(f"En total tenemos {animales_totales} animales disponibles para la adopción")
+        ver_animales()
+        print(" ")
     elif respuesta == 2:
-        print("¿Qué tipo de animal deseas adoptar?")
-        print("1. Perro")
-        print("2. Gato")
-        print("3. Pájaro")
-        tipo_animal = int(input())
-        
+        tipo_animal = adopcion()
+        print(" ")
         if tipo_animal == 1:
             if num_perros > 0:
                 num_perros -= 1
+                perros_adoptados += 1
                 print(f"Felicidades {nombre}, has adoptado un perro!")
                 print(" ")
             else:
@@ -46,6 +70,7 @@ while respuesta == 1 or respuesta == 2:
         elif tipo_animal == 2:
             if num_gatos > 0:
                 num_gatos -= 1
+                gatos_adoptados += 1
                 print(f"Felicidades {nombre}, has adoptado un gato!")
                 print(" ")
             else:
@@ -53,20 +78,18 @@ while respuesta == 1 or respuesta == 2:
         elif tipo_animal == 3:
             if num_pajaros > 0:
                 num_pajaros -= 1
+                pajaros_adoptados += 1
                 print(f"Felicidades {nombre}, has adoptado un pájaro!")
                 print(" ")
             else:
                 print("Lo siento, no tenemos pájaros disponibles para adopción.")
                  
-    print("Selecciona la opción que deseas:")
-    print("1. Ver animales disponibles")
-    print("2. Adoptar un animal")
-    print("3. Salir")
-    respuesta = int(input())
-if respuesta == 3:
-    print(f"Gracias por visitarnos {nombre}, vuelve pronto!")
-else:
-    print("Opción no válida.")
+    elif respuesta == 3:
+        print(f"Gracias por visitarnos {nombre}, vuelve pronto!")
+        print(" ")
+        print(f"Has adoptado {perros_adoptados} perros, {gatos_adoptados} gatos y {pajaros_adoptados} pájaros")
+    else:
+        print("Opción no válida.")
 
 print(" ")
 print("************************************")
